210c771ffda680d03ef6bf02fdf123771b093885
Abduljebar <Abduljebar49@gmail.com>
2022-02-25 20:45:08 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    loadChildren: () =>
+      import('./home/home-routing.module').then((m) => m.HomeRoutingModule),
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
